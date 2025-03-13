1dc29e267b7ca453802f5b7f1d859053bdcbc7c6
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-03-13 15:11:31 -0300

    add resource module to app module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AccountUsersResourceModule } from '@my-task-timer/account-users-resource';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AccountUsersResourceModule],
 })
-export class AppModule {}
+export class AppModule { }
