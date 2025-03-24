bdf7fd9d03d16d45654f342889d4afc7f4b97909
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-03-24 12:01:42 -0300

    add repository implementation methods

--- packages/account-users/data-source/src/lib/repository/auth.repository.impl.ts
+++ packages/account-users/data-source/src/lib/repository/auth.repository.impl.ts
@@ -2,20 +2,39 @@ import { Account, AuthRepository } from '@my-task-timer/account-users-domain';
 import { DRIZZLE_PROVIDER, schema } from '@my-task-timer/shared-data-source';
 import { Inject } from '@nestjs/common';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { ilike, or } from 'drizzle-orm';
+import { ilike, or, eq } from 'drizzle-orm';
 
 export class AuthRepositoryImpl implements AuthRepository {
   constructor(
     @Inject(DRIZZLE_PROVIDER) private readonly db: NodePgDatabase<typeof schema>
   ) {}
 
-  async createOne(input: Account) {
+  async createOne(input: Account): Promise<Account> {
     const { id, ...insertData } = input;
     const [createUser] = await this.db
       .insert(schema.users)
       .values(insertData)
       .returning();
     return createUser;
+  }
+
+  async findOne(id: string): Promise<Account> {
+    const [user] = await this.db
+      .select()
+      .from(schema.users)
+      .where(eq(schema.users.id, id));
+
+    return user;
+  }
+
+  async updateOne(id: string, input: Account): Promise<Account> {
+    const [updateUser] = await this.db
+      .update(schema.users)
+      .set(input)
+      .where(eq(schema.users.id, id))
+      .returning();
+
+    return updateUser;
   }
 
   async findByEmailOrUsername(
