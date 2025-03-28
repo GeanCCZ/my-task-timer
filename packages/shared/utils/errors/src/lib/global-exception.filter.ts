897698a020db3c839ba489a82a867072778e4efc
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-03-28 11:04:42 -0300

    update global exceptions filter

--- packages/shared/utils/errors/src/lib/global-exception.filter.ts
+++ packages/shared/utils/errors/src/lib/global-exception.filter.ts
@@ -6,6 +6,8 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CustomException } from './exceptions';
+import { HttpErrorType } from '@my-task-timer/shared-interfaces';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
@@ -16,10 +18,16 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
     let message = 'Internal server error';
+    let errorType: "UNAUTHORIZED" | "FORBIDDEN" | "BAD_REQUEST" | "NOT_FOUND" | "INTERNAL_SERVER_ERROR" | "CONFLICT" = HttpErrorType.INTERNAL_SERVER_ERROR;
 
-    if (exception instanceof HttpException) {
+    if (exception instanceof CustomException) {
       status = exception.getStatus();
       message = exception.message;
+      errorType = exception.errorType;
+    } else if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.message;
+      errorType = this.mapStandardErrorToType(exception);
     } else if (exception instanceof Error) {
       message = exception.message;
     }
@@ -28,8 +36,29 @@ export class GlobalExceptionFilter implements ExceptionFilter {
       error: true,
       status,
       message,
-      list: request.body || {}, // Include request body in 'list'
+      errorType,
+      list: request.body || {},
       timestamp: new Date().toISOString(),
     });
+  }
+
+  private mapStandardErrorToType(
+    exception: HttpException
+  ): keyof typeof HttpErrorType {
+    const status = exception.getStatus();
+    switch (status) {
+      case 400:
+        return HttpErrorType.BAD_REQUEST;
+      case 401:
+        return HttpErrorType.UNAUTHORIZED;
+      case 403:
+        return HttpErrorType.FORBIDDEN;
+      case 404:
+        return HttpErrorType.NOT_FOUND;
+      case 409:
+        return HttpErrorType.CONFLICT;
+      default:
+        return HttpErrorType.INTERNAL_SERVER_ERROR;
+    }
   }
 }
