52efc53e13620818ab067d51bf14d9fbf8d8fac1
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-04-02 17:00:10 -0300

    refactor find one

--- packages/account-users/domain/src/lib/usecases/find-one.ts
+++ packages/account-users/domain/src/lib/usecases/find-one.ts
@@ -3,6 +3,11 @@ import { UserDto } from '../dtos/user.dto';
 import { AccountRepository } from '../repository/account.repository';
 import { Injectable } from '@nestjs/common';
 import { UserMapper } from '../mappers/user.mapper';
+import {
+  InternalServerError,
+  NotFoundException,
+  tryCatch,
+} from '@my-task-timer/shared-utils-errors';
 
 @Injectable()
 export class FindOneUseCase implements Usecase<string, UserDto> {
@@ -12,7 +17,17 @@ export class FindOneUseCase implements Usecase<string, UserDto> {
   ) {}
 
   async execute(id: string): Promise<UserDto> {
-    const userDomain = await this.accountRepository.findOne(id);
+    const { data: userDomain, error: userError } = await tryCatch(
+      this.accountRepository.findOne(id)
+    );
+
+    if (userError || !userDomain) {
+      throw userError?.message.includes('not found')
+        ? new NotFoundException(`User with id ${id} does not exist`)
+        : new InternalServerError(
+            'An unexpected error occurred while retrieving the user'
+          );
+    }
 
     return this.userMapper.toDto(userDomain);
   }
