fdd499ca7d49e8a4d54f1b3f6ede8f843e9a93ec
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-04-02 16:07:11 -0300

    add exceptions to sign up

--- packages/account-users/domain/src/lib/usecases/sign-up.ts
+++ packages/account-users/domain/src/lib/usecases/sign-up.ts
@@ -8,6 +8,7 @@ import { SignUpDto } from '../dtos/sign-up.dto';
 import { SignUpMapper } from '../mappers/sign-up.mapper';
 import {
   InternalServerError,
+  ConflictException,
   tryCatch,
 } from '@my-task-timer/shared-utils-errors';
 import { Account } from '../entities/account.entity';
@@ -36,8 +37,13 @@ export class SignUpUseCase implements Usecase<SignUpDto, SignUpDto> {
 
     const accountDomain = this.signUpMapper.toEntity(domainInput);
 
+    return this.createAccount(accountDomain);
+  }
+
+  private async createAccount(account: Account): Promise<Account> {
     const { data: createdAccount, error: createdAccountError } = await tryCatch(
-      this.accountRepository.createOne(accountDomain)
+      this.accountRepository.createOne(account),
+      (e) => this.normalizeRepositoryError(e)
     );
 
     if (!createdAccount || createdAccountError) {
@@ -46,5 +52,21 @@ export class SignUpUseCase implements Usecase<SignUpDto, SignUpDto> {
       );
     }
     return this.signUpMapper.toResponse(createdAccount as Account);
+  }
+
+  private normalizeRepositoryError(error: unknown) {
+    if (this.isDuplicateEmailError(error))
+      throw new ConflictException('Email already exists');
+
+    throw new InternalServerError(
+      'Failed to create account: ACCOUNT_REPOSITORY_ERROR'
+    );
+  }
+
+  private isDuplicateEmailError(error: any): boolean {
+    return (
+      error?.code === '23505' || // PostgreSQL duplicate key
+      error?.status === 409
+    );
   }
 }
