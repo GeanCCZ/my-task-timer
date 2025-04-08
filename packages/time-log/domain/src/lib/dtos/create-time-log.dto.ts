74b7192b20f2484613e67e5dc677e2beeb72358d
GabrielL915 <gabrielluizgomes1@gmail.com>
2025-04-08 10:41:56 -0300

    add strict property initialization false

--- packages/time-log/domain/src/lib/dtos/create-time-log.dto.ts
+++ packages/time-log/domain/src/lib/dtos/create-time-log.dto.ts
@@ -4,7 +4,7 @@ import { Expose } from 'class-transformer';
 import { IsDateString, IsNotEmpty } from 'class-validator';
 
 export class CreateTimeLogDto {
-  @Expose()
+
   @IsDateString(
     {},
     { message: 'A data de início do log de tempo é obrigatória.' }
@@ -15,13 +15,12 @@ export class CreateTimeLogDto {
   })
   startedAt!: Date;
 
-  @Expose()
   @IsNotEmpty({ message: 'A tarefa relacionada deve ser informada.' })
   @ApiProperty({
     example: {
-      id: '123',
+      taskId: '123',
     },
     description: 'ID da tarefa relacionada ao log de tempo',
   })
-  task?: Task;
+  taskId: string;
 }
